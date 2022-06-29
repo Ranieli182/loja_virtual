66fdd208cea8208bb08b33a3d581be87356aec73
Ranieli Medeiros Vieira <97738185+Ranieli182@users.noreply.github.com>
2022-06-29 11:39:54 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter's Clothing",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+        Scaffold(
+          body: HomeTab(),
+          drawer: CustomDrawer(_pageController),
+        ),
+        Scaffold(
+         appBar: AppBar(
+           title: Text("Produtos"),
+           centerTitle: true,
+           backgroundColor: Color.fromARGB(255, 203, 236, 241),
+         ),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
+        Container(color: Colors.yellow,),
+        Container(color: Colors.green,),
+      ],
+    );
+  }
+}
